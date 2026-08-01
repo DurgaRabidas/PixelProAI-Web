2229da345c2eb463491b88cf45dbf75b82be8de2
DurgaRabidas <laxmirabidas9@gmail.com>
2026-08-01 20:47:59 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+const upload = document.getElementById("upload");
+const image = document.getElementById("image");
 
+if (upload && image) {
+    upload.addEventListener("change", function (event) {
+        const file = event.target.files[0];
+
+        if (file) {
+            image.src = URL.createObjectURL(file);
+            image.style.display = "block";
+        }
+    });
+}
