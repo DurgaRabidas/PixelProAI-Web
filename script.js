47c235fa7d9accd532039e98e8cc81bd2fb2be12
DurgaRabidas <laxmirabidas9@gmail.com>
2026-08-02 20:59:03 +0530

    Refactor file upload event listener in script.js

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
 const upload = document.getElementById("upload");
 const image = document.getElementById("image");
 
-if (upload && image) {
-    upload.addEventListener("change", function (event) {
-        const file = event.target.files[0];
+upload.addEventListener("change",function(e){
 
-        if (file) {
-            image.src = URL.createObjectURL(file);
-            image.style.display = "block";
-        }
-    });
+const file=e.target.files[0];
+
+if(file){
+
+image.src=URL.createObjectURL(file);
+
+image.style.display="block";
+
 }
+
+});
