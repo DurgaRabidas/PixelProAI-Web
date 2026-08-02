f5e465a2295052a555fcfe226ec8aebf4df3492b
DurgaRabidas <laxmirabidas9@gmail.com>
2026-08-02 21:18:34 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,32 +1,48 @@
 const upload = document.getElementById("upload");
 const image = document.getElementById("image");
 
+const brightness = document.getElementById("brightness");
+const contrast = document.getElementById("contrast");
+
 let rotation = 0;
 let flipX = 1;
+let brightnessValue = 100;
+let contrastValue = 100;
 
-upload.addEventListener("change", function (e) {
+function updateImage() {
+    image.style.transform = `rotate(${rotation}deg) scaleX(${flipX})`;
+    image.style.filter = `brightness(${brightnessValue}%) contrast(${contrastValue}%)`;
+}
+
+upload.addEventListener("change", function(e) {
     const file = e.target.files[0];
-
     if (file) {
         image.src = URL.createObjectURL(file);
         image.style.display = "block";
+        updateImage();
     }
 });
 
 document.querySelectorAll(".left-panel button").forEach(button => {
     button.addEventListener("click", () => {
-
         if (button.textContent === "Rotate") {
             rotation += 90;
-            image.style.transform =
-                `rotate(${rotation}deg) scaleX(${flipX})`;
         }
 
         if (button.textContent === "Flip") {
             flipX *= -1;
-            image.style.transform =
-                `rotate(${rotation}deg) scaleX(${flipX})`;
         }
 
+        updateImage();
     });
+});
+
+brightness.addEventListener("input", () => {
+    brightnessValue = brightness.value;
+    updateImage();
+});
+
+contrast.addEventListener("input", () => {
+    contrastValue = contrast.value;
+    updateImage();
 });
